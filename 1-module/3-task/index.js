eded2e9c91778e15788fe919588ea04d06512bc2
Alexey <alexey93kovalev@gmail.com>
2021-03-04 00:41:50 +0600

    first letter UC

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if(str == null)
+    return "";
+  let firstUCLetter = str.slice(0, 1).toUpperCase();
+  if(str.length == 1) 
+    return firstUCLetter;
+  return firstUCLetter + str.slice(1);
 }
