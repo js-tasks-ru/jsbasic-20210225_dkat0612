66b127fc556cca4588a114a33dea5cb1bc9041d3
Alexey <alexey93kovalev@gmail.com>
2021-03-04 01:02:49 +0600

    check string for spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  const SPAM_STRING_1 = "1xbet";
+  const SPAM_STRING_2 = "xxx";
+  if(str != null && (str.toLowerCase().includes(SPAM_STRING_1) 
+                  || str.toLowerCase().includes(SPAM_STRING_2)) )
+    return true;
+  return false;
 }
