4e120b60673f49c812f61101ccce4823bd489662
Alexey <alexey93kovalev@gmail.com>
2021-03-04 01:10:57 +0600

    truncated string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,6 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if(str === null || str.length <= maxlength)
+    return str;
+  const CONTINUED = "…";
+  return str.slice(0, maxlength - CONTINUED.length) + CONTINUED;
 }
