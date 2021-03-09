f7d29cb9347d288745a674fd236c8e1ba3768199
Alexey <alexey93kovalev@gmail.com>
2021-03-09 23:40:58 +0600

    Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (key in salaries) {
+    const val = salaries[key];
+    if(typeof val === 'number' && !isNaN(val) && 
+              val != -Infinity && val != Infinity){
+      sum += val;
+    }
+  }
+  return sum;
 }
