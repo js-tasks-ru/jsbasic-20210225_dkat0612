8f4d1246af59b98d8ccee55c02e6f0c27f6943a0
Alexey <alexey93kovalev@gmail.com>
2021-03-15 16:00:32 +0600

    camelize function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  const newStr = str.split('-')
+                      .map((word, index) => {
+                        if(index > 0 ){
+                          return word[0].toUpperCase() + word.slice(1);
+                        }
+                        return word;
+                      })
+                      .join('');
+  return newStr;
 }
