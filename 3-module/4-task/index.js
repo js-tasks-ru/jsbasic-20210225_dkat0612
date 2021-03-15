33174fa17ce1eb2178ca9e6862527c65043c33b8
Alexey <alexey93kovalev@gmail.com>
2021-03-15 16:24:46 +0600

    Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filteredUsers = users.filter(user => (user.age <= age))
+                              .map(({name, balance}) => ({name, balance}));
+  return filteredUsers.map( user => Object.values(user).join(', '))
+                      .join('\n');
 }
