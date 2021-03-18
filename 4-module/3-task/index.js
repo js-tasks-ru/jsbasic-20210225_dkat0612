3f37c2b892759efe988de1d032e60746dfcc2c4f
Alexey <alexey93kovalev@gmail.com>
2021-03-18 15:35:19 +0600

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+  let index  = {
+    status : 0,
+    gender : 0,
+    age : 0
+  };
+  const tHeadRow = table.tHead.rows[0].cells;
+  for(let i = 0; i < tHeadRow.length; ++i){
+    if(tHeadRow[i].innerHTML === 'Status'){
+      index.status = i;
+    }
+    if(tHeadRow[i].innerHTML === 'Gender'){
+      index.gender = i;
+    }
+    if(tHeadRow[i].innerHTML === 'Age'){
+      index.age = i;
+    }
+  }
+  for(let i = 1; i < table.rows.length; ++i){
+    let row = table.rows[i];
+    const available = row.cells[index.status].dataset.available;
+    if(available === undefined){
+      row.setAttribute('hidden', 'true');
+    } else{
+        row.classList.add((available === 'true' ? 'available' : 'unavailable'));
+    }
+    row.classList.add((row.cells[index.gender].innerHTML === 'f' ? 'female' : 'male'));
+    if(row.cells[index.age].innerHTML < 18){
+      row.setAttribute('style', 'text-decoration: line-through');
+    }
+  }
 }
