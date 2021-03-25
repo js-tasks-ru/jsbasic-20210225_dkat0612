e88c56211b0a9dc8c1cd0498b8c800adb6267ede
Alexey <alexey93kovalev@gmail.com>
2021-03-25 14:11:59 +0600

    Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
+const SLIDE_COUNT = 4;
+
 function initCarousel() {
-  // ваш код...
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const carouselInner = document.querySelector('.carousel__inner');
+  const innerWidth = carouselInner.offsetWidth;
+  let currentSlide = 0;
+  leftButton.style.display = 'none';
+
+  rightButton.addEventListener('click', () => {
+    carouselInner.style.transform = 'translateX(-' + innerWidth *  ++currentSlide + 'px)';
+    if(currentSlide === SLIDE_COUNT - 1){
+      rightButton.style.display = 'none';
+    }else {
+      rightButton.style.display = '';
+      leftButton.style.display = '';
+    }
+  });
+
+  leftButton.addEventListener('click', () => {
+    carouselInner.style.transform = 'translateX(-' + innerWidth *  --currentSlide + 'px)';
+    if(currentSlide === 0){
+      leftButton.style.display = 'none';
+    }else{
+      rightButton.style.display = '';
+      leftButton.style.display = '';
+    }
+  });
 }
