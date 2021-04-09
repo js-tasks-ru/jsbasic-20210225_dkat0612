1b3494b0fee17ed4e5fe686915515006dc034a62
Alexey <alexey93kovalev@gmail.com>
2021-04-09 13:03:47 +0600

    Учебный проект: Модальное окно

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,61 @@
 import createElement from '../../assets/lib/create-element.js';
 
+function modalTemplate() {
+  return `
+  <div class="modal">
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+
+        <h3 class="modal__title"></h3>
+      </div>
+
+      <div class="modal__body"></div>
+    </div>
+  </div>`
+}
+
 export default class Modal {
   constructor() {
+    this._elem = createElement(modalTemplate());
+    this._elem.querySelector('.modal__close').addEventListener('click', () => {
+        this._removeModal();
+    });
+    document.addEventListener('keydown', (event) => {
+      if(event.code === 'Escape'){
+        this._removeModal();
+      }
+    });
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
+  open(){
+    document.body.insertAdjacentElement('afterbegin', this._elem);
+    document.body.classList.add('is-modal-open');
+  }
+
+  setTitle(title){
+    this._elem.querySelector('.modal__title').innerText = title;
+  }
+
+  setBody(node){
+    let modalBody = this._elem.querySelector('.modal__body');
+    modalBody.innerHTML = '';
+    modalBody.insertAdjacentElement('afterbegin', node);
+  }
+
+  close(){
+    this._removeModal();
+  }
+
+  _removeModal(){
+    document.body.classList.remove('is-modal-open');
+    this._elem.remove();
   }
 }
